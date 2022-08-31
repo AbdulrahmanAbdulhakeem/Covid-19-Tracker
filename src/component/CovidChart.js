53adacc19327df9c6a07f05e965bfb86dad58eed
AbdulrahmanAbdulhakeem <abdulrahmanabdulhakeem952@gmail.com>
2022-08-31 13:18:51 +0000

    cleaning garbage code

--- src/component/CovidChart.js
+++ src/component/CovidChart.js
@@ -1,54 +1,35 @@
 import React, { useEffect } from "react";
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Chart } from 'react-chartjs-2'
-import {Line} from 'react-chartjs-2'
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
-function CovidChart({ date, deathCases, allCases , country , recover}) {
+function CovidChart({ date, deathCases, allCases, country, recover }) {
   useEffect(() => {
-    console.log(allCases)
-    console.log(date)
-    console.log(deathCases)
-  },[country])
+    console.log(allCases);
+    console.log(date);
+    console.log(deathCases);
+  }, [country]);
 
   const data = {
-    labels:date.map((dates) => dates),
-    datasets:[
+    labels: date.map((dates) => dates),
+    datasets: [
       {
-        label:'Cases of Covid',
-        data:allCases.map(cases => cases),
-        // borderColor:['#fa5157'],
-        backgroundColor:['red'],
-         // backgroundColor:[
-        //   'rgba(75,192,192,1)',
-        //   '#ecf0f1',
-        //   '#50AF95',
-        //   '#f3ba2f',
-        //   '#2a71d0',
-        // ],
-        // borderColor:'gray',
-        // borderWidth:2,
+        label: "Cases of Covid",
+        data: allCases.map((cases) => cases),
+        backgroundColor: ["red"],
       },
       {
-        label:'Death Cases',
-        data:deathCases.map(deaths => deaths),
-        // borderColor:['#f64c72'],
-        backgroundColor:['black'],
-        // backgroundColor:[
-        //   'rgba(75,192,192,1)',
-        //   '#ecf0f1',
-        //   '#50AF95',
-        //   '#f3ba2f',
-        //   '#2a71d0',
-        // ],
-        borderColor:'black',
-        borderWidth:2,
+        label: "Death Cases",
+        data: deathCases.map((deaths) => deaths),
+        backgroundColor: ["black"],
+        borderColor: "black",
+        borderWidth: 2,
       },
-      // 
-    ]
-  }
+    ],
+  };
 
   return (
-    <div className = 'chart'>
+    <div className="chart">
       <Line data={data} />
     </div>
   );
--- src/component/CovidChartDetails.js
+++ src/component/CovidChartDetails.js
@@ -9,12 +9,10 @@ function CovidChartDetails({ country }) {
   const [allCases, setAllCases] = useState([]);
   const [deathCases, setDeathCases] = useState([]);
   const [date, setDate] = useState([]);
-  const[recover , setRecover] = useState([])
 
   let dateContainer = [];
   let caseContainer = [];
   let deathContainer = [];
-  let recovered = []
 
   const fetchCountryChartDetails = () => {
     let myData = [];
@@ -27,7 +25,6 @@ function CovidChartDetails({ country }) {
         const { timeline } = myData;
         const { cases, deaths , recovered } = timeline;
         for (const date of Object.keys(cases)) {
-          // console.log(date)
           dateContainer.push(date);
         }
 
@@ -41,13 +38,7 @@ function CovidChartDetails({ country }) {
           deathContainer.push(death);
         }
         setDeathCases(deathContainer);
-        // console.log(deathContainer);
-        // console.log(dateContainer);
-        // dateContainer.map((date) => console.log(date));
-        // console.log(caseContainer);
-
-        // setAllCases(dateContainer)
-      })
+         })
       .catch((error) => {
         const errMsg = error.message;
         setError(errMsg);
@@ -61,7 +52,6 @@ function CovidChartDetails({ country }) {
 
   return (
     <div className="chart-container">
-      {/* {deathCases.map((death) => console.log(death))} */}
       <CovidChart date={date} deathCases={deathCases} allCases={allCases} country = {country} recover ={recover}/>
     </div>
   );
